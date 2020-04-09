37404c2064f679b05c0a1e91c33b0e875240196d
David Vilaça <vilacapdavid@gmail.com>
2020-04-09 09:19:39 -0300

    chore: test open browser window

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -4,6 +4,15 @@ module.exports = {
     port: 4444
   },
   launch: {
-    headless: process.env.HEADLESS !== 'false'
+    headless: process.env.HEADLESS !== 'false',
+    devtools: process.env.DEVTOOLS !== 'false',
+    defaultViewport: {
+      width: 1366,
+      height: 768,
+      deviceScaleFactor: 1,
+      isMobile: false,
+      hasTouch: false
+    },
+    args: ['--start-maximized']
   }
 }
