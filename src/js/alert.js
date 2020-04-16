b6a536963e96dea5f1458e519f09bc481faa394a
David Vilaça <vilacapdavid@gmail.com>
2020-04-15 23:28:38 -0300

    feat(alert): add timeout support

--- src/js/alert.js
+++ src/js/alert.js
@@ -1,12 +1,13 @@
 /**
  * Create a element alert and append in element
  * @param {Window} window Window DOM instance
- * @returns {(message: string, appendTo: HTMLElement, type?: 'default' | 'success' | 'error', id?: string) => HTMLElement}
+ * @returns {(message: string, appendTo: HTMLElement, type?: 'default' | 'success' | 'error', id?: string, temporary?: boolean, timeout?: number) => HTMLElement}
  */
-export const createAlertFactory = window => (message, appendTo, type = 'default', id = `alert_${Math.round(Math.random() * 1024)}`, temporary = true) => {
+export const createAlertFactory = window => (message, appendTo, type = 'default', id = `alert_${Math.round(Math.random() * 1024)}`, temporary = false, timeout = 10) => {
   const alertElement = window.document.createElement('div')
   const messageElement = window.document.createElement('p')
   const close = window.document.createElement('span')
+  alertElement.id = id
   alertElement.classList.add('alert-box', type, 'fade-in')
   close.classList.add('close-button')
   messageElement.innerText = message
@@ -25,6 +26,14 @@ export const createAlertFactory = window => (message, appendTo, type = 'default'
       appendTo.removeChild(alertElement)
     }
   })
+
+  if (temporary) {
+    setTimeout(() => {
+      if (!alertElement.classList.contains('fade-out')) {
+        close.click()
+      }
+    }, timeout * 1000)
+  }
 
   return alertElement
 }
